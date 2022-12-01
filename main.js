e9651abaa39c993b40cd9bd05f10e790f69b5021
Pablo <psolari21@hotmail.com>
2022-11-30 22:47:11 -0300

    Hecha la card

--- main.js
+++ main.js
@@ -3,37 +3,43 @@ const Pizzas = [
         id: 1,
         nombre: 'Muzzarella',
         ingredientes: ['Queso', 'Oregano'],
-        precio: 1000
+        precio: 1000,
+        img: './img/muzza.jpg'
     },
     {
         id: 2,
         nombre: 'Especial',
         ingredientes: ['Queso', 'Jamon'],
-        precio: 1200
+        precio: 1200,
+        img: './img/especial.jpg'
     },
     {
         id: 3,
         nombre: 'Napolitana',
         ingredientes: ['Queso', 'Tomate', 'Oregano'],
-        precio: 1600
+        precio: 1600,
+        img: './img/napo.jpg'
     },
     {
         id: 4,
         nombre: 'Anana',
         ingredientes: ['Jamon Crudo', 'Anana'],
-        precio: 1800
+        precio: 1800,
+        img: './img/anana.jpg'
     },
     {
         id: 5,
         nombre: 'Pollo',
         ingredientes: ['Queso', 'Jamon', 'Pollo'],
-        precio: 1650
+        precio: 1650,
+        img: './img/pollo.jpg'
     },
     {
         id: 6,
         nombre: 'Calabresa',
         ingredientes: ['Queso', 'Salamin'],
-        precio: 1400
+        precio: 1400,
+        img: './img/calabresa.jpg'
     },
 ]
 
@@ -42,42 +48,44 @@ const barra = '----------------------'
 
 const boton = document.getElementById("btn-obtener")
 const input = document.getElementById("id_pizza")
-const contenedor = document.querySelector("#container")
-const precio_error = document.querySelector("#precio_error")
-const nombre_pizza = document.querySelector("#pizza")
+const card = document.querySelector(".card_pizza")
 const form = document.querySelector(".form")
 
 console.log(boton)
 console.log(input)
-console.log(contenedor)
-console.log(precio_error)
-console.log(nombre_pizza)
+console.log(card)
+
 console.log(form)
 
-function obtenerPizza(event) {
+
+const obtenerPizza = (event)  => {
     event.preventDefault();
 
-    /*if (typeof(parseInt(input.value)) !== 'number'){
-        error = 'No es un número'
-    }*/
     const existePizza = Pizzas.find(pizza => pizza.id === parseInt(input.value));
 
-    if (typeof existePizza === 'undefined') {
-        nombre_pizza.textContent = ''
-        precio_error.textContent = `No existe la pizza`
-        return;
-    }
+    if (!existePizza) {
+        card.innerHTML =
+            `<div id="container">
+                <h2 id="pizza">No se encuentra la pizza.</h2>
+                <p id="ingredientes"></p>
+                <h3 id="precio_error"></h3>
+                <img id="img_pizza" src="" alt="">
+            </div>`
+            return;
+        }
+        
+    
+    card.innerHTML =
+            `<div id="container">
+                <h2 id="pizza">La pizza es ${existePizza.nombre}</h2>
+                <p id="ingredientes">Los ingredientes son: ${existePizza.ingredientes}</p>
+                <h3 id="precio_error">El precio es $ ${existePizza.precio}</h3>
+                <img id="img_pizza" src="${existePizza.img}" alt="">
+            </div>`;
 
-    nombre_pizza.textContent = `La pizza es ${existePizza.nombre}`
-    precio_error.textContent = `El precio es $ ${existePizza.precio}`
-
-}
-
-function limpiar(e) {
-    input.value = '';
 }
 
 form.onsubmit = obtenerPizza
-input.addEventListener("focus", limpiar)
+
 
 
